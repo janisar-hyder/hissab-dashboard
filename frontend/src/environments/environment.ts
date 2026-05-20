7fea8267f724253b37538a087a870733e834f776
thestearis <janisarhyder90@gmail.com>
2026-05-20 19:25:09 +0500

    Goes to production fully checked

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000/api/v1'
-  // apiUrl: 'http://tamezy01.ap-south-1.elasticbeanstalk.com/api/v1'
+  // apiUrl: 'http://tamezy102.ap-south-1.elasticbeanstalk.com/api/v1'
   apiUrl: '/api/v1'
 };
